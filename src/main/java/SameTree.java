cc2fcc4b4bf3b9b2daa8fedc58dd8dffd692abe6
Oksana Lahutina <oksankalahutina@gmail.com>
2023-06-28 19:20:59 +0300

    Same Tree implementation

---
+++ src/main/java/SameTree.java
@@ -0,0 +1,37 @@
+/*
+https://leetcode.com/problems/same-tree/description/?envType=study-plan-v2&envId=top-interview-150
+ */
+
+//  Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if ((p == null || q == null) || (p.val != q.val)) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
